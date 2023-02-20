28d6be36bfb47dc364968608343ab9c7cfeea8ad
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-02-20 13:13:26 +0100

    Store provider

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './screens/Home'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Choose' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ title: 'Choose' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
