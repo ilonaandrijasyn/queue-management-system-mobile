ba7040ed011c43e3abf3dbc8f7463cbac2a9886e
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-04-20 11:25:47 +0200

    Do not check for android manufacturer

--- helpers/device.ts
+++ helpers/device.ts
@@ -2,12 +2,8 @@ import * as Application from 'expo-application'
 import * as Device from 'expo-device'
 
 export const getPhoneId = async () => {
-  const manufacturer = Device.manufacturer
-  if (manufacturer === 'Apple') {
+  if (Device.manufacturer === 'Apple') {
     return await Application.getIosIdForVendorAsync()
   }
-  if (manufacturer === 'Google' || manufacturer === 'xiaomi') {
-    return Application.androidId
-  }
-  return null
+  return Application.androidId
 }
